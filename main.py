2ded23202915d1494f6c5047f38d5f204aee8a3e
Reno Wijoyo <renowijoyo@gmail.com>
2022-10-03 17:52:33 +0700

    remove datadog apm

--- main.py
+++ main.py
@@ -37,14 +37,14 @@ from flask import Blueprint
 
 import logging
 
-from datadog import initialize, statsd
-
-options = {
-    'statsd_host':'127.0.0.1',
-    'statsd_port':8125
-}
-
-initialize(**options)
+# from datadog import initialize, statsd
+#
+# options = {
+#     'statsd_host':'127.0.0.1',
+#     'statsd_port':8125
+# }
+#
+# initialize(**options)
 
 
 app = Flask(__name__)
