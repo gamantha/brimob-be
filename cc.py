1e2caa02e5a767d2ca69193e129b27e62059e0aa
Reno Wijoyo <renowijoyo@gmail.com>
2023-01-25 22:11:28 +0700

    add data count to active user

--- cc.py
+++ cc.py
@@ -215,7 +215,10 @@ def get_tracker_activeuser_byregion():
 
     cursor.execute(query,(region_id,))
     record = cursor.fetchall()
-    return jsonify(record)
+    result = dict()
+    result['data'] = record
+    result['count'] = cursor.rowcount
+    return jsonify(result)
 
 @cc_blueprint.route('/get_tracker_activeuser_byoperation', methods=["POST"])
 def get_tracker_activeuser_byoperation():
@@ -226,11 +229,14 @@ def get_tracker_activeuser_byoperation():
     query = "select max(tracker_user.id), tracker_user.iduser, user.username, user_data.nama, user_data.pangkat, region.region_name, department.department_name, position.position_name, lat, lon, altitude, tracker_user.timestamp " \
             "from tracker_user LEFT JOIN user on tracker_user.iduser = user.iduser LEFT JOIN position on user.position_id = position.id " \
             "LEFT JOIN user_data on user_data.iduser = tracker_user.iduser LEFT JOIN department on department.id = position.department_id LEFT JOIN region on region.id = department.region_id " \
-            "where tracker_user.iduser in (select user_id from operasi_anggota where status = 'active' and operasi_id = %s) AND `timestamp` > DATE_SUB(now(),INTERVAL 3 HOUR) group by tracker_user.iduser "
+            "where tracker_user.iduser in (select user_id from operasi_anggota where status = 'active' and operasi_id = %s) AND `timestamp` > DATE_SUB(now(),INTERVAL 3 YEAR) group by tracker_user.iduser "
 
     cursor.execute(query,(operation_id,))
     record = cursor.fetchall()
-    return jsonify(record)
+    result = dict()
+    result['data'] = record
+    result['count'] = cursor.rowcount
+    return jsonify(result)
 
 
 @cc_blueprint.route('/get_tracker_devices', methods=["GET"])
